33e67dbe6950ce7deda14f13985bb3119bee5fd3
Evan Cottingham <ejc1488@gmail.com>
2015-10-26 10:14:08 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+$("#change-content").click(function() {
+    var select = $("#selector").val();
+    var content = $("#newContent").val();
+    $(select).text(content);
+});
+
+$("#add-at-end").click(function() {
+    var select = $("#selector").val();
+    var content = $("#newContent").val();
+    $(select).append(content);
+});
+
+$("#add-at-start").click(function() {
+    var select = $("#selector").val();
+    var content = $("#newContent").val();
+    $(select).prepend(content);
+});
